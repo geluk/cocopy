54c1ad6e69087b08f3d3db51ccf8e3e828fb7782
Johan Geluk <johan@geluk.io>
2022-01-26 22:22:58 +0100

    Split Windows build scripts

--- build_win64.ps1
+++ build_win64.ps1
@@ -1,13 +1,2 @@
-Push-Location 'C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build'
-
-cmd /c "vcvars64.bat&set" |
-ForEach-Object {
-    if ($_ -match "=") {
-        $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-    }
-}
-Pop-Location
-
-Write-Host "Linker flags: " $env:CommonLinkerFlagsMSVC
-
+nasm -f win64 -o out/out.obj out/out.asm
 link out\out.obj /subsystem:console /out:hello.exe legacy_stdio_definitions.lib msvcrt.lib
