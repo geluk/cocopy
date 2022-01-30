281810f6323fa18bd7730091d398d1216e7ee34d
Johan Geluk <johan@geluk.io>
2022-01-30 21:23:42 +0100

    Fix IL documentation

--- src/il/mod.rs
+++ src/il/mod.rs
@@ -3,9 +3,9 @@
 //! In cocopy, intermediate code is represented as three-address code (TAC).
 //! As the name implies, each TAC instruction will take at most three operands,
 //! though some will take less. Often, instructions will take the form of
-//! `x = y <> z`, where `<>` represents some binary operand.
+//! `x = y <> z`, where `<>` represents some binary operation.
 //! Other instructions may take the form of `x = y` or `x = ~ y`, where `~` is
-//! a unary operand.
+//! a unary operator.
 //!
 //! Instructions are guaranteed to be in static single-assignment form (SSA).
 
