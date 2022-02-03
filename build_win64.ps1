d393a61f9d3a23889ecb2fe30716ba20cc21677b
Johan Geluk <johan@geluk.io>
2022-02-03 19:18:03 +0100

    Windows build script: emit debug symbols

--- build_win64.ps1
+++ build_win64.ps1
@@ -1,2 +1,2 @@
 ./lib/nasm-2.15.05/nasm.exe -f win64 -o out/out.obj out/out.asm
-link out\out.obj /subsystem:console /out:hello.exe legacy_stdio_definitions.lib msvcrt.lib
+link out\out.obj /debug /subsystem:console /out:hello.exe legacy_stdio_definitions.lib msvcrt.lib
